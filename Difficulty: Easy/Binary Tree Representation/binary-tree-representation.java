7f2b831aa5ba98b067ed80669a8fb2d49116c0ca
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2024-10-13 16:27:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Tree Representation/binary-tree-representation.java
+++ Difficulty: Easy/Binary Tree Representation/binary-tree-representation.java
@@ -78,11 +78,23 @@ class Solution{
     public static void createTree(Node root0, ArrayList<Integer> v ){
         // Code here
         
-        root0.left=new Node(v.get(1));
-        root0.right=new Node(v.get(2));
-        root0.left.left=new Node(v.get(3));
-        root0.left.right=new Node(v.get(4));
-        root0.right.left=new Node(v.get(5));
-        root0.right.right=new Node(v.get(6));
+        // root0.left=new Node(v.get(1));
+        // root0.right=new Node(v.get(2));
+        // root0.left.left=new Node(v.get(3));
+        // root0.left.right=new Node(v.get(4));
+        // root0.right.left=new Node(v.get(5));
+        // root0.right.right=new Node(v.get(6));
+        
+        Queue<Node> q=new LinkedList<>();
+        q.add(root0);
+        int i=1;
+        while(i<7)
+        {
+            Node temp=q.poll();
+            temp.left=new Node(v.get(i++));
+            q.add(temp.left);
+            temp.right=new Node(v.get(i++));
+            q.add(temp.right);
+        }
     }
 }
