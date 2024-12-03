860c7f12a88388a6eef1ec83302580adbc2856d9
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2024-12-03 19:51:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimal Cost/minimal-cost.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int minimizeCost(int k, int arr[]) {
+        // code here
+        int n=arr.length;
+        if(n==1) return 0;
+        if(n==2) return Math.abs(arr[1]-arr[0]);
+        int[] dp=new int[n];
+        dp[0]=0;
+        dp[1]=Math.abs(arr[1]-arr[0]);
+        for(int i=2;i<n;i++)
+        {
+            int mini=Integer.MAX_VALUE;
+            for(int j=1;j<=k &&j<=i;j++)
+            {
+                int s=dp[i-j]+Math.abs(arr[i]-arr[i-j]);
+                mini=Math.min(mini,s);
+            }
+            dp[i]=mini;
+            
+        }
+        return dp[n-1];
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.minimizeCost(k, arr);
+
+            System.out.println(res);
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
