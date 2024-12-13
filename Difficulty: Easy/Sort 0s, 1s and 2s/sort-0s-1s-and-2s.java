31807f826711326317cfc2a395b5907cdfc060d0
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2024-12-13 19:03:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String input = br.readLine();
+            String[] inputArray = input.split("\\s+");
+            int a[] = new int[inputArray.length];
+
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
+
+            Solution ob = new Solution();
+            ob.sort012(a);
+
+            for (int num : a) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+
+
+// } Driver Code Ends
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        int n=arr.length;
+        int lo=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(arr[mid]==0)
+            {
+                swap(arr,mid,lo);
+                mid++;
+                lo++;
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(arr,mid,high);
+                high--;
+            }
+        }
+        
+    }
+    public void swap(int[] arr, int i,int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
