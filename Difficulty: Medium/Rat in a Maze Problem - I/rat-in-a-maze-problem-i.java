93ba6ee2b9a9bb89ceeb209bc9ac976e3f4befee
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-01-09 00:04:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.java
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.java
@@ -51,46 +51,50 @@ class Solution {
     // Function to find all possible paths
     public ArrayList<String> findPath(ArrayList<ArrayList<Integer>> mat) {
         // code here
-        ArrayList<String> res=new ArrayList<>();
+        ArrayList<String> paths=new ArrayList<>();
         String path="";
         int n=mat.size();
         int m=mat.get(0).size();
         boolean[][] visited=new boolean[n][m];
-        solve(0,0,path,visited,res,mat);
-        return res;
+        solve(0,0,mat,visited,path,paths);
+        return paths;
     }
-    public void solve(int row,int col,String path,boolean[][] visited,ArrayList<String> res,ArrayList<ArrayList<Integer>> mat)
+    public void solve(int row,int col,ArrayList<ArrayList<Integer>>mat,boolean[][] visited,String path,ArrayList<String>paths)
     {
-        if(row==visited.length-1 && col==visited[0].length-1)
+        if(row==mat.size()-1 &&col==mat.get(0).size()-1)
         {
-            res.add(new String(path));
+            paths.add(path);
             return;
         }
         visited[row][col]=true;
-        if(safe(row+1,col,visited,mat))
+        if(isValid(row+1,col,mat,visited))
         {
-            solve(row+1,col,path+'D',visited,res,mat);
+            solve(row+1,col,mat,visited,path+"D",paths);
         }
-        if(safe(row,col-1,visited,mat))
+        if(isValid(row,col-1,mat,visited))
         {
-            solve(row,col-1,path+'L',visited,res,mat);
+            solve(row,col-1,mat,visited,path+"L",paths);
         }
-        if(safe(row,col+1,visited,mat))
+        if(isValid(row,col+1,mat,visited))
         {
-            solve(row,col+1,path+'R',visited,res,mat);
+            solve(row,col+1,mat,visited,path+"R",paths);
         }
-        if(safe(row-1,col,visited,mat))
+        if(isValid(row-1,col,mat,visited))
         {
-            solve(row-1,col,path+'U',visited,res,mat);
+            solve(row-1,col,mat,visited,path+"U",paths);
         }
+        
+        
         visited[row][col]=false;
-        
-        
     }
-    public boolean safe(int row,int col,boolean[][] visited,ArrayList<ArrayList<Integer>>mat)
+    public boolean isValid(int row,int col,ArrayList<ArrayList<Integer>>mat,boolean[][] visited)
     {
-        if(row<0 ||row>=mat.size() || col<0 || col>=mat.get(0).size()) return false;
-        if(visited[row][col]==true || mat.get(row).get(col)==0) return false;
+        int n=visited.length;
+        if(row<0 || row>=n) return false;
+        if(col<0 || col>=n) return false;
+        if(mat.get(row).get(col)==0) return false;
+        if(visited[row][col]) return false;
+        
         return true;
     }
 }
